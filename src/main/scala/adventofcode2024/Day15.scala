6033b2ec86f5c497759a588a96d833f0a2c95ae8
Peter Podlovics <peter.d.podlovics@gmail.com>
2024-12-15 23:08:23 +0100

    create separate objects for day 15 tasks

--- src/main/scala/adventofcode2024/Day15.scala
+++ src/main/scala/adventofcode2024/Day15.scala
@@ -1,7 +1,5 @@
 package adventofcode2024
 
-import adventofcode2024.Day15.WarehouseObject.Robot
-
 import scala.annotation.{nowarn, tailrec}
 import scala.collection.mutable
 import adventofcode2024.common.{Coords, iterateWhile}
@@ -9,38 +7,16 @@ import adventofcode2024.common.VectorVectorExtensions.*
 
 object Day15 {
   def main(args: Array[String]): Unit = {
-    println(task1())
-    println(task2())
+    println(Task1.run())
+    println(Task2.run())
   }
 
-  enum WarehouseObject:
-    case Wall, Empty, Box, Robot
-
-  import WarehouseObject.*
-
-  type WarehouseMap = Vector[Vector[WarehouseObject]]
-
-  case class Input(
-    warehouse: WarehouseMap,
-    moves: Vector[Coords],
-  )
-
-  def readInput(): Input = {
+  def readPreparsedInput(): (Vector[Vector[Char]], Vector[Coords]) = {
     val bufferedSource = io.Source.fromResource("day15.txt")
     val lines = bufferedSource.getLines.toVector
     bufferedSource.close
 
-    val (mapLines, _ +: movesLines) = lines.span(_.nonEmpty)
-
-    val warehouse = mapLines.map { line =>
-      line.toVector.map {
-        case '.' => Empty
-        case '#' => Wall
-        case 'O' => Box
-        case '@' => Robot
-      }
-    }
-
+    val (warehouseLines, _ +: movesLines) = lines.span(_.nonEmpty)
     val moves = movesLines.mkString.toVector.map {
       case '^' => Coords(-1, 0)
       case 'v' => Coords(1, 0)
@@ -48,103 +24,135 @@ object Day15 {
       case '<' => Coords(0, -1)
     }
 
-    Input(warehouse, moves)
+    (warehouseLines.map(_.toVector), moves)
   }
 
-  def findRobot(map: WarehouseMap): Coords = {
-    val x = map.indexWhere(_.contains(Robot))
-    val y = map(x).indexOf(Robot)
-    Coords(x, y)
-  }
+  object Task1 {
+    enum WarehouseObject:
+      case Wall, Empty, Box, Robot
 
-  def prettyPrintWarehouse(warehouse: WarehouseMap): Unit = {
-    warehouse.foreach { row =>
-      val prettyRow = row.map {
-        case Wall  => '#'
-        case Empty => '.'
-        case Box   => 'O'
-        case Robot => '@'
+    import WarehouseObject.*
+
+    type WarehouseMap = Vector[Vector[WarehouseObject]]
+
+    case class Input(
+      warehouse: WarehouseMap,
+      moves: Vector[Coords],
+    )
+
+    def readInput(): Input = {
+      val (warehouse, moves) = readPreparsedInput()
+
+      val parsedWarehouse = warehouse.map { row =>
+        row.map {
+          case '.' => Empty
+          case '#' => Wall
+          case 'O' => Box
+          case '@' => Robot
+        }
       }
-      println(prettyRow.mkString)
-    }
-  }
 
-  // assumes the map is surrounded by walls
-  // assumes there is at least one box in the direction of the move
-  def shiftBoxes(robotPos: Coords, warehouse: WarehouseMap, move: Coords): (Coords, WarehouseMap) = {
-    val dimX = warehouse.size
-    val dimY = warehouse.head.size
-    val firstBoxCoords@Coords(fstBoxX, fstBoxY) = robotPos + move
-    // could do this with an iterator
-    val boxCoordsInDirection = iterateWhile(firstBoxCoords) { pos =>
-      val newPos = pos + move
-      if (newPos.isWithinBounds(dimX, dimY) && warehouse(newPos.x)(newPos.y) == Box) {
-        Some(newPos)
-      } else {
-        None
+      Input(parsedWarehouse, moves)
+    }
+
+    def findRobot(map: WarehouseMap): Coords = {
+      val x = map.indexWhere(_.contains(Robot))
+      val y = map(x).indexOf(Robot)
+      Coords(x, y)
+    }
+
+    def prettyPrintWarehouse(warehouse: WarehouseMap): Unit = {
+      warehouse.foreach { row =>
+        val prettyRow = row.map {
+          case Wall => '#'
+          case Empty => '.'
+          case Box => 'O'
+          case Robot => '@'
+        }
+        println(prettyRow.mkString)
       }
     }
-    val firstNonBoxCoords@Coords(fstNonBoxX, fstNonBoxY) = boxCoordsInDirection.last + move
-    warehouse(fstNonBoxX)(fstNonBoxY) match {
-      case Wall =>
-        (robotPos, warehouse)
-      case Empty =>
-        val newWarehouse = warehouse
-          .updated(firstNonBoxCoords, Box)
-          .updated(firstBoxCoords, Robot)
-          .updated(robotPos, Empty)
-        (firstBoxCoords, newWarehouse)
-      case x =>
-        throw new Exception(s"Unexpected object. coords: ${firstNonBoxCoords}, object: '${x}'")
+
+    // assumes the map is surrounded by walls
+    // assumes there is at least one box in the direction of the move
+    def shiftBoxes(robotPos: Coords, warehouse: WarehouseMap, move: Coords): (Coords, WarehouseMap) = {
+      val dimX = warehouse.size
+      val dimY = warehouse.head.size
+      val firstBoxCoords@Coords(fstBoxX, fstBoxY) = robotPos + move
+      // could do this with an iterator
+      val boxCoordsInDirection = iterateWhile(firstBoxCoords) { pos =>
+        val newPos = pos + move
+        if (newPos.isWithinBounds(dimX, dimY) && warehouse(newPos.x)(newPos.y) == Box) {
+          Some(newPos)
+        } else {
+          None
+        }
+      }
+      val firstNonBoxCoords@Coords(fstNonBoxX, fstNonBoxY) = boxCoordsInDirection.last + move
+      warehouse(fstNonBoxX)(fstNonBoxY) match {
+        case Wall =>
+          (robotPos, warehouse)
+        case Empty =>
+          val newWarehouse = warehouse
+            .updated(firstNonBoxCoords, Box)
+            .updated(firstBoxCoords, Robot)
+            .updated(robotPos, Empty)
+          (firstBoxCoords, newWarehouse)
+        case x =>
+          throw new Exception(s"Unexpected object. coords: ${firstNonBoxCoords}, object: '${x}'")
+      }
+    }
+
+    def simulate(robotPos: Coords, warehouse: WarehouseMap, move: Coords): (Coords, WarehouseMap) = {
+      val dimX = warehouse.size
+      val dimY = warehouse.head.size
+      val newRobotPos@Coords(newRX, newRY) = robotPos + move
+
+      val newRobotsPosObject = warehouse(newRX)(newRY)
+      // assuming the map is surrounded by walls
+      newRobotsPosObject match {
+        case Wall => (robotPos, warehouse)
+        case Empty => (newRobotPos, warehouse.updated(newRobotPos, Robot).updated(robotPos, Empty))
+        case Box => shiftBoxes(robotPos, warehouse, move)
+      }
+    }
+
+    def calcGpsCoord(coords: Coords): Int = {
+      coords.x * 100 + coords.y
+    }
+
+    def collectBoxCoords(warehouse: WarehouseMap): Vector[Coords] = {
+      val dimX = warehouse.size
+      val dimY = warehouse.head.size
+      val coords = for {
+        x <- 0 until dimX
+        y <- 0 until dimY
+        if warehouse(x)(y) == Box
+      } yield Coords(x, y)
+      coords.toVector
+    }
+
+    def run(): Int = {
+      val Input(warehouse, moves) = readInput()
+      val robotPos = findRobot(warehouse)
+
+      val (finalRobotPos, finalWarehouse) = moves.foldLeft(robotPos -> warehouse) { case ((curRobotPos, curWarehouse), move) =>
+        simulate(curRobotPos, curWarehouse, move)
+      }
+
+      println(robotPos)
+      println(moves)
+      prettyPrintWarehouse(finalWarehouse)
+
+      collectBoxCoords(finalWarehouse).map(calcGpsCoord).sum
     }
   }
 
-  def simulate(robotPos: Coords, warehouse: WarehouseMap, move: Coords): (Coords, WarehouseMap) = {
-    val dimX = warehouse.size
-    val dimY = warehouse.head.size
-    val newRobotPos@Coords(newRX, newRY) = robotPos + move
-
-    val newRobotsPosObject = warehouse(newRX)(newRY)
-    // assuming the map is surrounded by walls
-    newRobotsPosObject match {
-      case Wall  => (robotPos, warehouse)
-      case Empty => (newRobotPos, warehouse.updated(newRobotPos, Robot).updated(robotPos, Empty))
-      case Box   => shiftBoxes(robotPos, warehouse, move)
-    }
-  }
-
-  def calcGpsCoord(coords: Coords): Int = {
-    coords.x * 100 + coords.y
-  }
-
-  def collectBoxCoords(warehouse: WarehouseMap): Vector[Coords] = {
-    val dimX = warehouse.size
-    val dimY = warehouse.head.size
-    val coords = for {
-      x <- 0 until dimX
-      y <- 0 until dimY
-      if warehouse(x)(y) == Box
-    } yield Coords(x, y)
-    coords.toVector
-  }
-
-  def task1(): Int = {
-    val Input(warehouse, moves) = readInput()
-    val robotPos = findRobot(warehouse)
-
-    val (finalRobotPos, finalWarehouse) = moves.foldLeft(robotPos -> warehouse) { case ((curRobotPos, curWarehouse), move) =>
-      simulate(curRobotPos, curWarehouse, move)
+  object Task2 {
+    def run(): Int = {
+      val fsIndex = readPreparsedInput()
+      42
     }
 
-    println(robotPos)
-    println(moves)
-    prettyPrintWarehouse(finalWarehouse)
-
-    collectBoxCoords(finalWarehouse).map(calcGpsCoord).sum
-  }
-
-  def task2(): Int = {
-    val fsIndex = readInput()
-    42
   }
 }
